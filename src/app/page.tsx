67dd4a32ee8af2981a873ccb465bc7837bb516d0
Bojenko <max.bozhenko@gmail.com>
2023-10-15 16:59:33 +0300

    finished phone

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,31 @@ import styles from './page.module.css'
 import {ConfigProvider} from "antd";
 import {theme} from "@/theme";
 import Header from "@/components/Header/Header";
+import CloverResearchTitle from "@/components/CRTitle/CloverResearchTitle";
 
 export default function Home() {
+    let titleSizes = {height: 200, width: 275};
     return (
         <ConfigProvider theme={theme}>
             <Header></Header>
             <main className={styles.main}>
                 <div className={styles.background}>
+                    <div className={styles.greenlines}>
+                        <div className={styles.titlecontainer}>
+                                <div className={styles.CRTitle}>
+                                    <CloverResearchTitle height={titleSizes.height} width={titleSizes.width}/>
+                                </div>
+                                <div className={styles.phone}>
+                                </div>
+
+                        </div>
+                        <div className={styles.bluerect}>
+                            <div></div>
+                            <div></div>
+                        </div>
+                    </div>
+
+
                 </div>
             </main>
         </ConfigProvider>
